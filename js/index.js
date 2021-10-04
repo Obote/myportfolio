ae7bfd1e52f6eade337bfca21189c74d70dc47f5
Leonardo Albornoz <albornozleo95@gmail.com>
2021-10-04 16:34:36 -0400

    Add menu responsiveness
    I added a second button to the HTML with an x icon. Also, I added a variable with the primary color
    and changed the position to the navbar to fixed so it displays at the top of the intro-section.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 const ham = document.getElementById('toggleButton');
+const close = document.getElementById('close');
+const title = document.getElementById('title');
 const men = document.getElementById('menu');
+const listItems = document.getElementsByClassName('list-item');
 
+const menuHandler = () => {
+    men.classList.toggle('menu-on');
+    close.classList.toggle('hide');
+    ham.classList.toggle('hide');
+    title.classList.toggle('hide');
+}
 
-ham.addEventListener('click', () => {
-    men.classList.toggle('bgshow');
-});
+ham.addEventListener('click', menuHandler);
+close.addEventListener('click', menuHandler);
+for(let item of listItems) {
+    item.addEventListener('click', menuHandler);
+}
