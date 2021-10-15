007444fe2ad66f0714d82aa04f8686d1084bd626
Obote <obotedenis256@gmail.com>
2021-10-15 23:12:34 +0300

    Review changes

--- javascript/index.js
+++ javascript/index.js
@@ -3,12 +3,27 @@ const formVal = document.getElementById('formV');
 const emailInput = document.getElementById('mail');
 
 formVal.addEventListener('submit', (event) => {
-  const validMail = /[A-Z]/g;
+  event.preventDefault();
 
-  if (validMail.test(emailInput.value)) {
-    document.getElementById('lowerCase').classList.toggle('formShow');
-    event.preventDefault();
-  } else if (!document.getElementById('lowerCase').className === 'mailError formShow') {
-    document.getElementById('lowerCase').classList.toggle('formShow');
+  const capitalLetters = [];
+
+  const emailCheck = emailInput.value.split('');
+  for (let i = 0; i < emailCheck.length; i += 1) {
+    if (!/[a-z]/.test(emailCheck[i]) && /[A-Z]/.test(emailCheck[i])) {
+      capitalLetters.push(emailCheck[i]);
+    }
   }
+
+  function errorMessage() {
+    const error = document.getElementById('valEmail');
+    error.textContent = 'Please enter email in lowercase';
+    error.style.color = 'red';
+  }
+
+  function correctEmail() {
+    formVal.submit();
+  }
+
+  if (capitalLetters.length > 0) errorMessage();
+  else correctEmail();
 });
