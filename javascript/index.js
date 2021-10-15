980225bdf3ee4d6b2f919eee0a523e839ce0cdd5
Obote <obotedenis256@gmail.com>
2021-10-15 19:10:43 +0300

    correct errors

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,14 @@
 // email validation
-
 const formVal = document.getElementById('formV');
 const emailInput = document.getElementById('mail');
 
 formVal.addEventListener('submit', (event) => {
-    const validMail = /[A-Z]/g;
+  const validMail = /[A-Z]/g;
 
-    if(validMail.test(emailInput.value)){
-        document.getElementById('lowerCase').classList.toggle('formShow');
-        event.preventDefault();
-    }else if (!document.getElementById('lowerCase').className === 'mailError formShow'){
-        document.getElementById('lowerCase').classList.toggle('formShow');
-    }
+  if (validMail.test(emailInput.value)) {
+    document.getElementById('lowerCase').classList.toggle('formShow');
+    event.preventDefault();
+  } else if (!document.getElementById('lowerCase').className === 'mailError formShow') {
+    document.getElementById('lowerCase').classList.toggle('formShow');
+  }
 });
